fc390e5f478bd8f6d0cf609473a46a5076c2ebeb
sukanya1426 <rahmanmahdiya2002@gmail.com>
2025-03-03 16:47:09 +0600

    deleted unnecessary test file

--- droidbot/hello.py
+++
@@ -1,8 +0,0 @@
-from SocketClient import SocketClient
-import asyncio
-
-async def main():
-    await SocketClient.connect()
-    SocketClient.send_message('message_from_client', {'verdict': 'fail', "response": 'ok'})
-
-asyncio.run(main())
